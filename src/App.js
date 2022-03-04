727d3bc6411eb8a877577909ccd7536e173642be
jamillypl <jamillyp@alu.ufc.br>
2022-03-03 22:19:17 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './assets/svg/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Oi, tem nada aqui ainda, mas o Luan é muito gostoso e eu to com saudade :(
+    </div>
+  );
+}
+
+export default App;
